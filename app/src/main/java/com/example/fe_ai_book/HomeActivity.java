77932084c9cfa130a18e4391bd9ae54ed187f72c
NOTEBOOK\cube3 <yeah3302@gmail.com>
2025-08-01 01:26:14 +0900

    메인홈.xml 초안

---
+++ app/src/main/java/com/example/fe_ai_book/HomeActivity.java
@@ -0,0 +1,19 @@
+package com.example.fe_ai_book;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_home);
+
+
+    }
+}
--- app/src/main/java/com/example/fe_ai_book/MainActivity.java
+++ app/src/main/java/com/example/fe_ai_book/MainActivity.java
@@ -17,11 +17,12 @@ public class MainActivity extends AppCompatActivity {
         Button signup = findViewById(R.id.signup_view);
         Button userInfo = findViewById(R.id.btn_user_info);
         Button mybookRecent = findViewById(R.id.btn_mybook_recent);
+        Button btn_home = findViewById(R.id.btn_home);
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MemberSignUp.class);
+                Intent intent = new Intent(getApplicationContext(), MemberSignUpActivity.class);
                 startActivity(intent);
             }
         });
@@ -38,6 +39,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MyBookRecentActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
                 startActivity(intent);
             }
         });
