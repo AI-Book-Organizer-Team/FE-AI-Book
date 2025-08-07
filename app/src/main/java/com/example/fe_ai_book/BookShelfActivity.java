aa7b2279178663fdb80f9bcc06508ffb964ecf63
NOTEBOOK\cube3 <yeah3302@gmail.com>
2025-08-07 22:21:52 +0900

    책장 만들기 & 메인액티비티 레이이아웃 수정

---
+++ app/src/main/java/com/example/fe_ai_book/BookShelfActivity.java
@@ -0,0 +1,19 @@
+package com.example.fe_ai_book;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.jar.Attributes;
+
+public class BookShelfActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookshelf);
+    }
+
+} //mainActivity
